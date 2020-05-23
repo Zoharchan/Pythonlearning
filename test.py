c1fc9e6e19757c4bee884177998adee597f55fe3
Chen Zhihao <czh331440742@gmail.com>
2020-05-23 23:17:21 +0800

    ceshi

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#encoding: utf-8
+'''
+@File    :   test.py
+@Time    :   2020/05/23 23:15:06
+@Author  :   Chen Zhihao 
+@Version :   1.0
+@Contact :   czh331440742@gmail.com
+@Desc    :   None
+'''
+
+# here put the import lib
+"zheshiyige ceshi"
